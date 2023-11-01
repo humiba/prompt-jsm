f43d9c1488fda8fd757ac64cb34eae16a33765da
humiba <huaminhbao7a4@gmail.com>
2023-11-01 14:50:47 +0700

    finish home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "@styles/globals.css";
+import { Navbar, Provider } from "@components";
 
 export const metadata = {
   title: "Promptopia",
@@ -11,11 +12,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">{children}</main>
+          <main className="app">
+            <Navbar />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
